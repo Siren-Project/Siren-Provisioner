b4852473a0d24a70331893c35443ba5aa3c65226
Lyndon John Fawcett <lyndonfawcett@hotmail.com>
2016-10-05 19:35:10 +0100

    Improved startup time

--- Discovery.py
+++ Discovery.py
@@ -1,5 +1,5 @@
 from Device import *
-
+import threading
 #Service that discovers new devices. In future work this will be split out into a separate server.
 class Discovery:
     ips = ["192.168.2.13", "192.168.2.14", "192.168.2.15", "192.168.2.16", "192.168.2.17", "192.168.2.18"] # read from file
@@ -12,10 +12,17 @@ class Discovery:
 
     def discover_devices(self):
         for ip in self.ips:
-            #We need to determine if device is discovered correctly. If not, periodically look for it
-            d = Device(ip, ip)
-            if(d.info):
-                self.devices.append(d)
+            #We need to determine if device is discovered correctly. If not, periodically look for it.
+            #Why might be not want to thread this? Device list could change.
+            threading.Thread(target=self.discover_device, args=(ip, ip)).start()
+            #d = Device(ip, ip)
+            #if(d.info):
+               # self.devices.append(d)
+
+    def discover_device(self, ip, id):
+        d = Device(ip, id)
+        if (d.info):
+            self.devices.append(d)
 
     def get_devices(self):
         return self.devices
